4e9459a230778f1412dcf3386e35ccb9689f1f3c
jvictor7070 <joao.v.coutinho7@ba.estudante.senai.br>
2026-03-30 16:49:43 -0300

    save

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        <div className="mensagem">
+            <h1>Olá, seja bem-vindo ao meu projeto.</h1>
+            <p>Este é um componente com uma mensagem simples.</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Mensagem
